3c402e033e77ae81179cb50e2b9771716edcd840
Craig Dietrich <craigd@Craigs-MacBook-Air.local>
2016-08-16 16:42:48 -0700

    Parsers

--- starter.profile.js
+++ starter.profile.js
@@ -1,22 +1,25 @@
 {
-  "archives":[ 
+  "archives":{ 
     {
       "title":"YouTube",
+      "identifier":"youtube",
       "subtitle":"Hosts user-generated videos. Includes network and professional content.",
       "categories":["video"],
       "thumbnail":"youtube.thumb.png"         		
     },
     {
       "title":"Vimeo",
+      "identifier":"vimeo",
       "subtitle":"Upload, store, share and manage HD videos.",
       "categories":["video"],
       "thumbnail":"vimeo.thumb.png"         		
     },
     {
       "title":"Internet Archive",
+      "identifier":"internetarchive",
       "subtitle":"The Internet Archive is a non-profit digital library of just about anything.",
       "categories":["video","image","audio","affiliated"],
       "thumbnail":"internetarchive.thumb.png"         		
     },
-  ]
+  }
 }
