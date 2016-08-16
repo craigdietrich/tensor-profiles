85ea39a3ada81ebc1dec3af117e505163420d8a7
Craig Dietrich <craigd@Craigs-MacBook-Air.local>
2016-08-16 16:27:05 -0700

    Starter profile

---
+++ starter.profile.js
@@ -0,0 +1,22 @@
+{
+  "archives":[ 
+    {
+      "title":"YouTube",
+      "subtitle":"Hosts user-generated videos. Includes network and professional content.",
+      "categories":["video"],
+      "thumbnail":"youtube.thumb.png"         		
+    },
+    {
+      "title":"Vimeo",
+      "subtitle":"Upload, store, share and manage HD videos.",
+      "categories":["video"],
+      "thumbnail":"vimeo.thumb.png"         		
+    },
+    {
+      "title":"Internet Archive",
+      "subtitle":"The Internet Archive is a non-profit digital library of just about anything.",
+      "categories":["video","image","audio","affiliated"],
+      "thumbnail":"internetarchive.thumb.png"         		
+    },
+  ]
+}
