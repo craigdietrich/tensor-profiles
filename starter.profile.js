3d8d6197b9b6a12f3abc89c282d0aacbd2a4acb8
Craig Dietrich <craigd@WL-193-11.WPA.Claremont.Edu>
2016-08-17 12:57:25 -0700

    Profile

--- starter.profile.js
+++ starter.profile.js
@@ -5,21 +5,21 @@
       "identifier":"youtube",
       "subtitle":"Hosts user-generated videos. Includes network and professional content.",
       "categories":["video"],
-      "thumbnail":"youtube.thumb.png"         		
+      "thumbnail":"https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/youtube/youtube.thumb.png"         		
     },
     {
       "title":"Vimeo",
       "identifier":"vimeo",
       "subtitle":"Upload, store, share and manage HD videos.",
       "categories":["video"],
-      "thumbnail":"vimeo.thumb.png"         		
+      "thumbnail":"https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/vimeo/vimeo.thumb.png"         		
     },
     {
       "title":"Internet Archive",
       "identifier":"internetarchive",
       "subtitle":"The Internet Archive is a non-profit digital library of just about anything.",
       "categories":["video","image","audio","affiliated"],
-      "thumbnail":"internetarchive.thumb.png"         		
+      "thumbnail":"https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/internetarchive/internetarchive.thumb.png"         		
     },
   }
 }
