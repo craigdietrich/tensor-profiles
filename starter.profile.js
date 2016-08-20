dbc9e488347b92bb31f5993b8c44a5e185f04c00
Craig Dietrich <craigd@Craigs-MacBook-Air.local>
2016-08-19 20:03:48 -0700

    JSONP

--- starter.profile.js
+++ starter.profile.js
@@ -1,25 +1,25 @@
 profile({
-  "archives":{ 
-    {
-      "title":"YouTube",
-      "identifier":"youtube",
-      "subtitle":"Hosts user-generated videos. Includes network and professional content.",
-      "categories":["video"],
-      "thumbnail":"https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/youtube/youtube.thumb.png"         		
-    },
-    {
-      "title":"Vimeo",
-      "identifier":"vimeo",
-      "subtitle":"Upload, store, share and manage HD videos.",
-      "categories":["video"],
-      "thumbnail":"https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/vimeo/vimeo.thumb.png"         		
-    },
-    {
-      "title":"Internet Archive",
-      "identifier":"internetarchive",
-      "subtitle":"The Internet Archive is a non-profit digital library of just about anything.",
-      "categories":["video","image","audio","affiliated"],
-      "thumbnail":"https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/internetarchive/internetarchive.thumb.png"         		
-    },
-  }
-})
+  "archives": [
+      {
+		"title": "YouTube",
+		"identifier": "youtube",
+		"subtitle": "Hosts user-generated videos. Includes network and professional content.",
+		"categories": ["video"],
+		"thumbnail": "https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/youtube/youtube.thumb.png"
+	  }, 
+      {
+		"title": "Vimeo",
+		"identifier": "vimeo",
+		"subtitle": "Upload, store, share and manage HD videos.",
+		"categories": ["video"],
+		"thumbnail": "https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/vimeo/vimeo.thumb.png"
+	  }, 
+      {
+		"title": "Internet Archive",
+		"identifier": "internetarchive",
+		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
+		"categories": ["video", "image", "audio", "affiliated"],
+		"thumbnail": "https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/internetarchive/internetarchive.thumb.png"
+	}
+  ]
+});
