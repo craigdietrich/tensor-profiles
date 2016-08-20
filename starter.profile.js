71f965ef4dd09c971c84c136393b250e6e28b373
Craig Dietrich <craigd@Craigs-MacBook-Air.local>
2016-08-19 21:51:34 -0700

    Critical Commons

--- starter.profile.js
+++ starter.profile.js
@@ -1,6 +1,6 @@
 profile({
-  "name":"Starter Profile",
-  "archives": [
+   "name":"Starter Profile",
+   "archives": [
       {
 		"title": "YouTube",
 		"identifier": "youtube",
@@ -21,6 +21,13 @@ profile({
 		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
 		"categories": ["video", "image", "audio", "affiliated"],
 		"thumbnail": "https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/internetarchive/internetarchive.thumb.png"
-	}
-  ]
+	  },
+      {
+		"title": "Critical Commons",
+		"identifier": "criticalcommons",
+		"subtitle": "For Fair &amp; Critical Participation in Media Culture",
+		"categories": ["video", "image", "affiliated"],
+		"thumbnail": "https://github.com/craigdietrich/tensor-profiles/raw/master/parsers/criticalcommons/criticalcommons.thumb.png"
+	  }
+   ]
 });
