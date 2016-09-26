9968424c9b9ce1d4293a3ec55430cd1b5bb2e390
Craig Dietrich <craigdietrich@gmail.com>
2016-09-26 15:15:49 -0700

    Update starter.profile.js

--- starter.profile.js
+++ starter.profile.js
@@ -4,25 +4,25 @@ profile({
    "archives": [
       {
 		"title": "YouTube",
-		"url":"https://github.com/craigdietrich/tensor-profiles/tree/master/parsers/youtube",
+		"url":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/parsers/youtube",
 		"subtitle": "Hosts user-generated videos. Includes network and professional content.",
 		"categories": ["video"]
 	  }, 
       {
 		"title": "Vimeo",
-		"url":"https://github.com/craigdietrich/tensor-profiles/tree/master/parsers/vimeo",
+		"url":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/parsers/vimeo",
 		"subtitle": "Upload, store, share and manage HD videos.",
 		"categories": ["video"]
 	  }, 
       {
 		"title": "Internet Archive",
-		"url":"https://github.com/craigdietrich/tensor-profiles/tree/master/parsers/internetarchive",
+		"url":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/parsers/internetarchive",
 		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
 		"categories": ["video", "image", "audio", "affiliated"]
 	  },
       {
 		"title": "Critical Commons",
-		"url":"https://github.com/craigdietrich/tensor-profiles/tree/master/parsers/criticalcommons",
+		"url":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/parsers/criticalcommons",
 		"subtitle": "For Fair &amp; Critical Participation in Media Culture",
 		"categories": ["video", "image", "affiliated"]
 	  }
