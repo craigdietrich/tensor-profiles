9626937822850e716fcf3744d8de04d96823fe6a
Craig Dietrich <craigdietrich@gmail.com>
2016-09-26 15:42:46 -0700

    Update starter.profile.js

--- starter.profile.js
+++ starter.profile.js
@@ -4,29 +4,25 @@ profile({
    "archives": [
       {
 		"title": "YouTube",
-		"zip":"https://github.com/craigdietrich/tensor-profiles/archive/master.zip",
-	        "path":"tensor-profiles/parsers/youtube",
+	        "parser":"youtube",
 		"subtitle": "Hosts user-generated videos. Includes network and professional content.",
 		"categories": ["video"]
 	  }, 
       {
 		"title": "Vimeo",
-		"zip":"https://github.com/craigdietrich/tensor-profiles/archive/master.zip",
-	        "path":"tensor-profiles/parsers/vimeo",
+		"parser":"vimeo",
 		"subtitle": "Upload, store, share and manage HD videos.",
 		"categories": ["video"]
 	  }, 
       {
 		"title": "Internet Archive",
-		"zip":"https://github.com/craigdietrich/tensor-profiles/archive/master.zip",
-	        "path":"tensor-profiles/parsers/internetarchive",
+		"parser":"internetarchive",
 		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
 		"categories": ["video", "image", "audio", "affiliated"]
 	  },
       {
 		"title": "Critical Commons",
-		"zip":"https://github.com/craigdietrich/tensor-profiles/archive/master.zip",
-	        "path":"tensor-profiles/parsers/criticalcommons",
+		"parser":"criticalcommons",
 		"subtitle": "For Fair &amp; Critical Participation in Media Culture",
 		"categories": ["video", "image", "affiliated"]
 	  }
