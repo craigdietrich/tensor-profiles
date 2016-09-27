b3fef48784f52401899765305ae14e9841470683
Craig Dietrich <craigdietrich@gmail.com>
2016-09-26 20:37:51 -0700

    Update testing.profile.js

--- testing.profile.js
+++ testing.profile.js
@@ -7,24 +7,6 @@ profile({
 	        "parser":"youtube",
 		"subtitle": "Hosts user-generated videos. Includes network and professional content.",
 		"categories": ["video"]
-	  }, 
-      {
-		"title": "Vimeo",
-		"parser":"vimeo",
-		"subtitle": "Upload, store, share and manage HD videos.",
-		"categories": ["video"]
-	  }, 
-      {
-		"title": "Internet Archive",
-		"parser":"internetarchive",
-		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
-		"categories": ["video", "image", "audio", "affiliated"]
-	  },
-      {
-		"title": "Critical Commons",
-		"parser":"criticalcommons",
-		"subtitle": "For Fair &amp; Critical Participation in Media Culture",
-		"categories": ["video", "image", "affiliated"]
 	  }
    ]
 });
