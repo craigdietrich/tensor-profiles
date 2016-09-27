40fc080b21c24a2c318edcc45e8ded33b38b35d7
Craig Dietrich <craigdietrich@gmail.com>
2016-09-27 09:59:07 -0700

    Scalar parser placeholder

--- starter.profile.js
+++ starter.profile.js
@@ -1,34 +1,41 @@
 profile({
-   "name":"Starter Profile",
-   "uri":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/starter.profile.js",
-   "archives": [
-      {
-		"title": "YouTube",
-	        "parser":"youtube",
-	        "url":"https://www.googleapis.com/youtube/v3",
-		"subtitle": "Hosts user-generated videos. Includes network and professional content.",
-		"categories": ["video"]
-	  }, 
-      {
+	"name":"Starter Profile",
+	"uri":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/starter.profile.js",
+	"archives": [
+	{
+    	  "title": "YouTube",
+    	  "parser":"youtube",
+    	  "url":"https://www.googleapis.com/youtube/v3",
+    	  "subtitle": "Hosts user-generated videos. Includes network and professional content.",
+    	  "categories": ["video"]
+	}, 
+	{
 		"title": "Vimeo",
 		"parser":"vimeo",
-	        "url":"https://api.vimeo.com",
+		"url":"https://api.vimeo.com",
 		"subtitle": "Upload, store, share and manage HD videos.",
 		"categories": ["video"]
-	  }, 
-      {
+	}, 
+	{
 		"title": "Internet Archive",
 		"parser":"internetarchive",
-	        "url":"https://archive.org",
+		"url":"https://archive.org",
 		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
 		"categories": ["video", "image", "audio", "affiliated"]
-	  },
-      {
+	},
+	{
 		"title": "Critical Commons",
 		"parser":"criticalcommons",
-	        "url":"http://criticalcommons.org",
+		"url":"http://criticalcommons.org",
 		"subtitle": "For Fair &amp; Critical Participation in Media Culture",
 		"categories": ["video", "image", "affiliated"]
-	  }
-   ]
+	},
+	{
+		"title": "Scalar 2 User's Guide",
+		"parser":"scalar",
+		"url":"http://scalar.usc.edu/works/guide2",
+		"subtitle": "This user's guide includes introductions to Scalar's features as well as more in-depth material to explore as your get to know the tools.",
+		"categories": ["video", "image", "affiliated", "audio"]
+	}
+	]
 });
