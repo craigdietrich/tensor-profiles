8a4656da448a4df1d6d7b18daacda31b4b891120
Craig Dietrich <craigdietrich@gmail.com>
2016-09-27 15:52:17 -0700

    Critical Commons reduc

--- parsers/criticalcommons/handler.php
+++ parsers/criticalcommons/handler.php
@@ -1,8 +1,11 @@
 <?php
+
+$url = 'http://criticalcommons.org/cc/playlist?SearchableText='.$query;
+
 // Generic HTTP handler
-session_start(); 
+session_start();
 $opts = array('http' => array('header'=> 'Cookie: ' . @$_SERVER['HTTP_COOKIE']."\r\n"));
 $context = stream_context_create($opts);
 session_write_close();
-$content = file_get_contents($source_uri, false, $context);
+$content = file_get_contents($url, false, $context);
 ?>
