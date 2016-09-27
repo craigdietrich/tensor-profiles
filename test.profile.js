dcd0000b4708146470684eb943d17203c7d2903d
Craig Dietrich <craigdietrich@gmail.com>
2016-09-26 19:14:04 -0700

    Create test.profile.js

---
+++ test.profile.js
@@ -0,0 +1,30 @@
+{
+   "name":"Starter Profile",
+   "uri":"https://raw.githubusercontent.com/craigdietrich/tensor-profiles/master/starter.profile.js",
+   "archives": [
+      {
+		"title": "YouTube",
+	        "parser":"youtube",
+		"subtitle": "Hosts user-generated videos. Includes network and professional content.",
+		"categories": ["video"]
+	  }, 
+      {
+		"title": "Vimeo",
+		"parser":"vimeo",
+		"subtitle": "Upload, store, share and manage HD videos.",
+		"categories": ["video"]
+	  }, 
+      {
+		"title": "Internet Archive",
+		"parser":"internetarchive",
+		"subtitle": "The Internet Archive is a non-profit digital library of just about anything.",
+		"categories": ["video", "image", "audio", "affiliated"]
+	  },
+      {
+		"title": "Critical Commons",
+		"parser":"criticalcommons",
+		"subtitle": "For Fair &amp; Critical Participation in Media Culture",
+		"categories": ["video", "image", "affiliated"]
+	  }
+   ]
+}
