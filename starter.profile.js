52f15926fe9003f4b3f90ebc6103eff57f32042a
Craig Dietrich <craigdietrich@gmail.com>
2016-10-08 13:57:05 -0700

    Crossroads

--- starter.profile.js
+++ starter.profile.js
@@ -50,7 +50,7 @@ profile({
 		"parser":"crossroads",
 		"url":"https://crossroads.oxy.edu",
 		"subtitle": "Global Crossroads is Occidental College's public forum for sharing ideas and media on matters of global relevance.",
-		"categories": ["video", ,"image", "audio", "affiliated"]
+		"categories": ["video", "image", "audio", "affiliated"]
 	}
 	]
 });
