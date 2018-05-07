7561c15d88ae62e447aa4cc52c0c20ba2c292091
Craig Dietrich <craigdietrich@gmail.com>
2018-05-07 14:26:21 -0700

    eBird audio and video

--- parsers/ebird/handler.php
+++ parsers/ebird/handler.php
@@ -2,10 +2,18 @@
 if ($single) {
 	// TODO
 } else {
-	//$url = 'https://ebird.org/media/catalog?q='.$query;
+	$media_type = 'all';
+	if (stristr($query, 'audio')) {
+		$media_type = 'a';
+		$query = str_replace('audio', '', $query);
+	} elseif (stristr($query, 'video')) {
+		$media_type = 'v';
+		$query = str_replace('video', '', $query);
+	}
 	$count = 50;
 	$start = ($page-1) * 50;
-	$url = 'https://ebird.org/media/catalog.json?searchField=species&q='.$query.'&taxonCode=&hotspotCode=&regionCode=&customRegionCode=&userId=&_mediaType=on&mediaType=all&species=&region=&hotspot=&customRegion=&mr=M1TO12&bmo=1&emo=12&yr=YALL&by=1900&ey=2018&user=&view=Gallery&sort=upload_date_desc&_tag=on&_tag=on&_req=on&subId=&catId=&_spec=on&specId=&dsu=-1&count='.$count.'&start='.$start;
+	$url = 'https://ebird.org/media/catalog.json?searchField=species&q='.rawurlencode($query).'&taxonCode=&hotspotCode=&regionCode=&customRegionCode=&userId=&_mediaType=on&mediaType='.$media_type.'&species=&region=&hotspot=&customRegion=&mr=M1TO12&bmo=1&emo=12&yr=YALL&by=1900&ey=2018&user=&view=Gallery&sort=upload_date_desc&_tag=on&_tag=on&_req=on&subId=&catId=&_spec=on&specId=&dsu=-1&count='.$count.'&start='.$start;
+	// https://ebird.org/media/catalog.json?searchField=species&q=&taxonCode=&hotspotCode=&regionCode=&customRegionCode=&userId=&_mediaType=on&mediaType=v&species=&region=&hotspot=&customRegion=&mr=M1TO12&bmo=1&emo=12&yr=YALL&by=1900&ey=2018&user=&view=Gallery&sort=upload_date_desc&_tag=on&_tag=on&_req=on&subId=&catId=&_spec=on&specId=&dsu=-1&start=0&_=1525726278306
 }
 
 // Generic HTTPS handler
